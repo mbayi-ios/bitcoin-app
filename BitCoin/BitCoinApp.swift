bd2cb6502b1dbe74e24b2ea558c98e92ff63f4b4
Ambrose Mbayi <91916741+mbayi-ios@users.noreply.github.com>
2023-07-30 11:29:46 +0300

    Initial Commit

---
+++ BitCoin/BitCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitCoinApp.swift
+//  BitCoin
+//
+//  Created by Ambrose Mbayi on 30/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct BitCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
